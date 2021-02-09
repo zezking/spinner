6268a2bccf5e2067fc55d70ed32da39f1b812111
zezking <zhaoenze001@gmail.com>
2021-02-09 20:00:50 +0000

    create a simple spinner animation

--- spinner.js
+++ spinner.js
@@ -15,6 +15,22 @@ const spinner = () => {
   setTimeout(() => {
     process.stdout.write("\r|   ");
   }, 900);
+  setTimeout(() => {
+    process.stdout.write("\r|   ");
+  }, 1100);
+  setTimeout(() => {
+    process.stdout.write("\r/   ");
+  }, 1300);
+  setTimeout(() => {
+    process.stdout.write("\r-   ");
+  }, 1500);
+  setTimeout(() => {
+    // Need to escape the backslash since it's a special character.
+    process.stdout.write("\r\\   ");
+  }, 1700);
+  setTimeout(() => {
+    process.stdout.write("\r|   " + "\n");
+  }, 1900);
 };
 
 spinner();
